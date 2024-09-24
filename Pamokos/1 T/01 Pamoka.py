c8c6fc965b327a9584b5ba306a1696c032a48a29
BitaiIrBaitai <150124893+BitaiIrBaitai@users.noreply.github.com>
2024-09-24 18:41:05 +0300

    Update 01 Pamoka.py

--- Pamokos/1 T/01 Pamoka.py
+++ Pamokos/1 T/01 Pamoka.py
@@ -1,1 +1,21 @@
-print("Hello world")
+# Kintamieji tai atminties vieta, kurioje saugomi duomenys.
+# Kintamieji turi pavadinimą, reikšmę ir tipą.
+
+# Kintamųjų tipai:
+# int - sveikasis skaičius
+# float - skaičius su kableliu / realus skaičius
+# str - tekstas
+# bool - loginis kintamasis (True / False)
+
+# Jei mes norime sukurti kintamąjį,
+# tai mes tiesiog parašome jo pavadinimą ir priskiriame reikšmę.
+
+# Kintamųjų pavadinimai:
+# Kintamieji gali būti sudaryti iš raidžių, skaičių ir _ simbolio.
+# Kintamieji negali prasidėti skaičiumi.
+# Kintamieji negali būti pavadinimai, kurie yra rezervuoti programavimo kalbos (if, else, while, for, ...).
+# Kintamieji gali būti jautrūs didžiosioms ir mažosioms raidėms. (pvz. kintamasis ir Kintamasis yra skirtingi kintamieji)
+
+# Kintamųjų priskyrimas:
+a = 5
+print(a)  # 5
