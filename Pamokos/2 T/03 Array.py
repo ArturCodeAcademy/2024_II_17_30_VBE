952ea3d84777f88f1777fb4300df93c446804381
ArturCodeAcademyKids <123232198+ArturCodeAcademy@users.noreply.github.com>
2024-12-10 18:40:37 +0200

    Update 03 Array.py

--- Pamokos/2 T/03 Array.py
+++ Pamokos/2 T/03 Array.py
@@ -46,3 +46,59 @@ for i in range(len(arr)):  # range(len(arr)) - sukuriame skaičių seką nuo 0 i
 arr = list()  # tas pats kas arr = []
 print(arr)  # []
 
+# Kartais reikia atlikti operacijas su masyvais.
+# Pavyzdžiui, pridėti papildomą elementą į masyvą,
+# ištrinti elementą iš masyvo, surasti elementą masyve ir t.t.
+
+arr = [1, 2, 3, 4, 5]
+print(arr)  # [1, 2, 3, 4, 5]
+
+# Pridėti elementą į masyvą galime naudojant append() metodą
+
+arr.append(6)  # Pridedame elementą 6 į masyvą
+print(arr)  # [1, 2, 3, 4, 5, 6]
+
+# Ištrinti elementą iš masyvo galime naudojant remove() metodą
+arr.remove(3)  # Ištriname elementą 3 iš masyvo
+print(arr)  # [1, 2, 4, 5, 6]
+
+# Surasti elementą masyve galime naudojant index() metodą
+print(arr.index(4))  # 2 - elementas 4 yra antroje pozicijoje
+
+# Panaikinti elementą iš masyvo pagal indeksą galime naudojant pop() metodą
+arr.pop(2)  # Panaikiname elementą 4 iš masyvo
+print(arr)  # [1, 2, 5, 6]
+
+# Prideti elementą pagal indeksą galime naudojant insert() metodą
+arr.insert(2, 10)  # Pridedame elementą 4 į masyvą antroje pozicijoje
+print(arr)  # [1, 2, 10, 5, 6]
+
+# Surikiuoti masyvą galime naudojant sort() metodą
+arr.sort()  # Surikiuojame masyvą
+print(arr)  # [1, 2, 5, 6, 10]
+
+# Atvirkštinę masyvo tvarką galime gauti naudojant reverse() metodą
+arr.reverse()  # Apverčiame masyvą
+print(arr)  # [10, 6, 5, 2, 1]
+
+# Pridėti kitą masyvą galime naudojant extend() metodą
+arr2 = [7, 8, 9]
+arr.extend(arr2)  # Pridedame masyvą arr2 į masyvą arr
+print(arr)  # [10, 6, 5, 2, 1, 7, 8, 9]
+
+# Taip pat galime naudoti + operatorių
+# Tai sujungs du masyvus į vieną
+arr3 = [11, 12, 13]
+print(arr2 + arr3)  # [7, 8, 9, 11, 12, 13]
+
+# Prie dabartinio masyvo galime pridėti kitą masyvą naudojant +=
+arr += arr3  # Pridedame masyvą arr3 prie masyvo arr
+print(arr)  # [10, 6, 5, 2, 1, 7, 8, 9, 11, 12, 13]
+
+# Taip pat galime naudoti * operatorių
+# Tai pakartos masyvą nurodytą skaičių kartų
+print(arr2 * 3)  # [7, 8, 9, 7, 8, 9, 7, 8, 9]
+
+# Išvalyti masyvą galime naudojant clear() metodą
+arr.clear()  # Išvalome masyvą
+print(arr)  # []
